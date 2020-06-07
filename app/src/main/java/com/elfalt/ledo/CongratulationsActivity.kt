f2670b895275022bd651be980e88841309fd0374
Hish17 <54224567+Hish17@users.noreply.github.com>
2020-06-07 23:11:25 +0200

    congratulations screen

---
+++ app/src/main/java/com/elfalt/ledo/CongratulationsActivity.kt
@@ -0,0 +1,19 @@
+package com.elfalt.ledo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+
+
+class CongratulationsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_congratulation)
+
+        //Congrats. Screen
+        //
+
+
+    }
+}
