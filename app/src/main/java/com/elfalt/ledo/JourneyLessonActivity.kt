96c439a22c981d576c55e16ce3b4e73bdf1929e4
HasanElfalt <hassanelfalt60@gmail.com>
2020-06-10 22:20:26 +0200

    modify JourneyLesson Activity again and videoView is working
    - videoView is working with NestedScrollView and the videoDetails fits the page.

--- app/src/main/java/com/elfalt/ledo/JourneyLessonActivity.kt
+++ app/src/main/java/com/elfalt/ledo/JourneyLessonActivity.kt
@@ -1,28 +1,62 @@
 package com.elfalt.ledo
 
+import android.media.MediaPlayer
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.MediaController
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_journey_lesson.*
 
 class JourneyLessonActivity : AppCompatActivity() {
 
+    private var playBackPosition = 0
+    private val videoPath : String = "android.resource://com.elfalt.ledo/${R.raw.kuka}"
+    private lateinit var mediaController : MediaController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_journey_lesson)
 
-        val videoPath : String = "android.resource://$packageName/${R.raw.americans_are_specific}"
+
+        video.setOnPreparedListener {
+            mediaController.setAnchorView(video)
+            video.setMediaController(mediaController)
+
+            video.seekTo(playBackPosition)
+            video.start()
+        }
+        
+        video.setOnInfoListener { _, what, _ ->
+            if(what == MediaPlayer.MEDIA_INFO_VIDEO_RENDERING_START)
+            progressBar.visibility = View.INVISIBLE
+            true
+        }
+
+        //Toast.makeText(this,"Duration is ${video.duration}, ${R.raw.kuka}",Toast.LENGTH_SHORT).show()
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        mediaController = MediaController(this)
         val uri = Uri.parse(videoPath)
         video.setVideoURI(uri)
+        
+        progressBar.visibility = View.VISIBLE
+    }
 
-        val medianController = MediaController(this)
-        video.setMediaController(medianController)
-        medianController.setAnchorView(video)
+    override fun onPause() {
+        super.onPause()
 
-        video.start()
+        video.pause()
+        playBackPosition = video.currentPosition
+    }
 
-        Toast.makeText(this,"Duration is ${video.duration}, ${R.raw.americans_are_specific}",Toast.LENGTH_SHORT).show()
+    override fun onStop() {
+        video.stopPlayback()
+        super.onStop()
     }
 }
