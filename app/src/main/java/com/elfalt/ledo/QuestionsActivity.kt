2d55e9fe8f7a1cd7080c29eccbe2f0e5a878aa3d
HasanElfalt <hassanelfalt60@gmail.com>
2020-06-11 16:24:49 +0200

    Add Questions Activity, some of its assets and add the dependency of circularImageView

---
+++ app/src/main/java/com/elfalt/ledo/QuestionsActivity.kt
@@ -0,0 +1,12 @@
+package com.elfalt.ledo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class QuestionsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_questions)
+    }
+}
