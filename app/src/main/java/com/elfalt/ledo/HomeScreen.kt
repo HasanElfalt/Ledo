60668de99ed4d52850da4cc6d872fda3e2c4a034
Mahmoud <mahmoudahmed.razek@gmail.com>
2020-06-13 01:48:33 +0200

    Edit MainActivity

--- app/src/main/java/com/elfalt/ledo/HomeScreen.kt
+++ app/src/main/java/com/elfalt/ledo/HomeScreen.kt
@@ -8,5 +8,7 @@ class HomeScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_screen)
+
+
     }
 }
--- app/src/main/java/com/elfalt/ledo/MainActivity.kt
+++ app/src/main/java/com/elfalt/ledo/MainActivity.kt
@@ -3,6 +3,9 @@ package com.elfalt.ledo
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.SpannableString
+import android.text.style.UnderlineSpan
+import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -10,6 +13,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
 
         getStarted.setOnClickListener {
             val intent = Intent(this, JourneyActivity :: class.java)
