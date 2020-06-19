4b60efb16c0793fa699f08d92697e10a48f36bc8
HasanElfalt <hassanelfalt60@gmail.com>
2020-06-19 20:24:24 +0200

    add shared preference feature to save data

--- app/src/main/java/com/elfalt/ledo/ui/JourneyActivity.kt
+++ app/src/main/java/com/elfalt/ledo/ui/JourneyActivity.kt
@@ -1,5 +1,6 @@
 package com.elfalt.ledo.ui
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.lifecycle.Observer
@@ -19,16 +20,37 @@ class JourneyActivity : AppCompatActivity() {
         setContentView(R.layout.activity_journey)
         origin = intent.getStringExtra("origin")
 
+        val sharedPref = getSharedPreferences("lessonFinished",Context.MODE_PRIVATE)
+        val defaultValue = "none"
+        val savedData = sharedPref.getString("save", defaultValue)
+
+
         lessons_recycler_view.layoutManager = LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false)
 
         lessonLessonViewModel = ViewModelProvider(this).get(LessonViewModel::class.java)
 
-        when(origin){
+        if(origin == "Home")    updateUI(savedData)
+        else    updateUI(origin)
+
+        lessonLessonViewModel.getLessonData().observe(this, Observer {
+
+            lessons_recycler_view.adapter = LessonsAdapter(it)
+        })
+
+    }
+
+    override fun onBackPressed() {
+        if(origin == "Home")
+            super.onBackPressed()
+    }
+    private fun updateUI(save :String?){
+
+        when(save){
             "Lesson 1"-> {
                 lessonLessonViewModel.lessonTwo = true
                 info_core.setImageResource(R.drawable.ic_book_shelf_lesson2)
                 core_values.alpha = 1.0F
-                }
+            }
             "Lesson 2"-> {lessonLessonViewModel.lessonTwo   = true
                 info_core.setImageResource(R.drawable.ic_book_shelf_lesson2)
                 core_values.alpha = 1.0F
@@ -45,25 +67,14 @@ class JourneyActivity : AppCompatActivity() {
                 info_goals.setImageResource(R.drawable.ic_book_shelf_lesson_finished)
                 goals_aspiration.alpha = 1.0F}
             "Lesson 4" ->{lessonLessonViewModel.lessonTwo   = true
-                    info_core.setImageResource(R.drawable.ic_book_shelf_lesson2)
-                    core_values.alpha = 1.0F
-                    lessonLessonViewModel.lessonThree = true
-                    info_strength.setImageResource(R.drawable.ic_book_shelf_lesson3)
-                    strength_weakness.alpha = 1.0F
-                    lessonLessonViewModel.lessonFour  = true
-                    info_goals.setImageResource(R.drawable.ic_book_shelf_lesson_finished)
-                    goals_aspiration.alpha = 1.0F }
+                info_core.setImageResource(R.drawable.ic_book_shelf_lesson2)
+                core_values.alpha = 1.0F
+                lessonLessonViewModel.lessonThree = true
+                info_strength.setImageResource(R.drawable.ic_book_shelf_lesson3)
+                strength_weakness.alpha = 1.0F
+                lessonLessonViewModel.lessonFour  = true
+                info_goals.setImageResource(R.drawable.ic_book_shelf_lesson_finished)
+                goals_aspiration.alpha = 1.0F }
         }
-
-        lessonLessonViewModel.getLessonData().observe(this, Observer {
-
-            lessons_recycler_view.adapter = LessonsAdapter(it)
-        })
-
-    }
-
-    override fun onBackPressed() {
-        if(origin == "Home")
-            super.onBackPressed()
     }
 }
